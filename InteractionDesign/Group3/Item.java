50589d3fbaf6b50c9123f810d544f417c00f6c8f
Gordon Buck <gordonbuck@dhcp-10-248-97-158.eduroam.wireless.private.cam.ac.uk>
2017-05-16 14:48:55 +0100

    ...

--- InteractionDesign/Group3/Item.java
+++
@@ -1,9 +0,0 @@
-package InteractionDesign.Group3;
-
-/**
- * Represents an item which may be recommended, depending on the weather
- * forecast.
- */
-public enum Item {
-	BAG_COVER, COAT, GLOVES, HELMET, LIGHT, SEAT_COVER, SUNGLASSES;
-}
--- InteractionDesign/Group3/Warning.java
+++
@@ -1,9 +0,0 @@
-package InteractionDesign.Group3;
-
-/**
- * Represents a weather warning.
- * We need to finalise the set of warnings.
- */
-public enum Warning {
-	ICY, STORMY, SNOWY, HIGH_WINDS;
-}
